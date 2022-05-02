e50d6e8075d5dc22bdc90d7ef5f940258f65d867
zHekLeR <zHekLeR@hotmail.com>
2022-05-01 22:45:37 -0400

    Heroku Git

---
+++ games/bigvanish.js
@@ -0,0 +1,110 @@
+// Import Pool for accessing the database.
+import pg from 'pg';
+const { Pool } = pg;
+const pool = new Pool({
+  connectionString: process.env.DATABASE,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+
+// Function to randomly timeout the user between 100-1,000,000 seconds.
+async function bigVanish(id) {
+    try {
+
+        // Get value and construct response string.
+        let rand = Math.floor(Math.random()*999000)+100;
+        let str = `/timeout ${id} ${rand} BIG Vanish! Random timeout between 100 and 1,000,000 seconds. `;
+        
+        // Pull user from database.
+        let client = await pool.connect();
+        let res = await client.query(`SELECT * FROM bigvanish WHERE user_id = '${id}';`);
+        let person = res.rows[0];
+        
+        if (!person) {
+
+            // User is not in the database. Add them.
+            person = { user_id: id, vanish: rand, lowest: rand };
+            await client.query(`INSERT INTO bigvanish(user_id, vanish, lowest)VALUES('${person.user_id}', ${person.vanish}, ${person.lowest});`);
+
+        } else {
+
+            // Update database if new timeout is their highest.
+            if (rand > person.vanish) {
+                await client.query(`UPDATE bigvanish SET vanish = ${rand} WHERE user_id = '${id}';`);
+                person.vanish = rand;
+            } 
+            
+            if (rand < person.lowest) {
+                await client.query(`UPDATE bigvanish SET lowest = ${rand} WHERE user_id = '${id}';`);
+                person.lowest = rand;
+            }
+
+        }
+        
+        // Release client.
+        client.release();
+    
+        // Return response.
+        return `${str}Your record high is ${person.vanish} seconds and low is ${person.lowest} seconds!`;
+
+    } catch (err) {
+        console.log(err);
+        return;
+    }
+  }
+
+
+  // Function to retrieve the leaderboard.
+  async function bigVanishLb() {
+    try {
+
+        // Pull users from the database.
+        let client = await pool.connect();
+        let res = await client.query(`SELECT * FROM bigvanish ORDER BY vanish DESC LIMIT 3;`);
+        let top = res.rows;
+        client.release();
+    
+        // Format top users.
+        let str = [];
+        for (let i = 0; i < top.length; i++) {
+            str.push(`${top[i].user_id}: ${top[i].vanish} seconds`);
+        }
+
+        // Return response.
+        return `Big Vanish Leaderboard | ${str.length?str.join(' | '):'No users have played bigvanish yet'}`;
+    } catch (err) {
+        console.log(err);
+        return;
+    }
+  }
+
+
+// Function to retrieve the lowest timeouts.
+async function bigVanishLow() {
+    try {
+
+        // Pull users from the database.
+        let client = await pool.connect();
+        let res = await client.query(`SELECT * FROM bigvanish ORDER BY lowest ASC LIMIT 3;`);
+        let top = res.rows;
+        client.release();
+    
+        // Format top users.
+        let str = [];
+        for (let i = 0; i < top.length; i++) {
+            if (top[i].lowest === 1000000) continue;
+            str.push(`${top[i].user_id}: ${top[i].lowest} seconds`);
+        }
+
+        // Return response.
+        return `Big Vanish Lowest Leaderboard | ${str.length?str.join(' | '):'No users have played bigvanish yet'}`;
+    } catch (err) {
+        console.log(err);
+        return;
+    }
+}
+
+
+  export { bigVanish, bigVanishLb, bigVanishLow };
---
+++ games/coinflip.js
@@ -0,0 +1,126 @@
+// Import Pool for accessing the database.
+import pg from 'pg';
+const { Pool } = pg;
+const pool = new Pool({
+  connectionString: process.env.DATABASE,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+
+// Constants.
+const choices = { "h": 1, "heads": 1, "t": 0, "tails": 0 };
+
+
+// Function to flip a coin. If the user guesses the result incorrectly, they get timed out for 1 minute.
+async function coinflip(id, input) {
+  try {
+
+    // Declare variable..
+    let choice;
+
+    // Determine if input is valid.
+    if (Object.keys(choices).includes(input.toLowerCase())) {
+      choice = choices[input.toLowerCase()];
+    } else {
+      return `@${id}: ${input} is not a correct input.`;
+    }
+
+    // Determine outcome.
+    let rand = Math.floor(Math.random()*2);
+    let shoot = `${rand==choice?'/me '+id+' has guessed correctly!':'/timeout '+id+' 60 You did NOT guess correctly!'}`;
+
+    // Pull user from the database.
+    let client = await pool.connect();
+    let res = await client.query(`SELECT * FROM coinflip WHERE user_id = '${id}';`);
+    let person = res.rows[0];
+
+    if (!person) {
+
+      // User has not played before. Add stats to database.
+      person = { user_id: id, correct: rand==choice?1:0, wrong: rand==choice?0:1 };
+      await client.query(`INSERT INTO coinflip(user_id, correct, wrong)VALUES('${person.user_id}', ${person.correct}, ${person.wrong});`);
+
+    } else {
+
+      // Update user's stats in database.
+      person.correct += rand==choice?1:0;
+      person.wrong += rand==choice?0:1;
+      await client.query(`UPDATE coinflip SET ${rand==choice?'correct':'wrong'} = ${rand==choice?person.correct:person.wrong} WHERE user_id = '${id}';`);
+
+    }
+
+    // Release client.
+    client.release();
+
+    // Return response.
+    return shoot + ` ${id} has guessed correctly ${person.correct} time${person.correct==1?'':'s'} and wrong ${person.wrong} time${person.wrong==1?'':'s'}!`;
+
+  } catch (err) {
+    console.log(err);
+    return;
+  }
+}
+
+
+// Function to get user's stats.
+async function coinflipScore(id) {
+  try {
+
+    // Pull user from the database.
+    let client = await pool.connect();
+    let res = await client.query(`SELECT * FROM coinflip WHERE user_id = '${id}';`);
+    let person = res.rows[0];
+    client.release();
+
+    // Declare response string.
+    let str;
+    if (person) {
+
+      // Format user's stats.
+      str = `${person.user_id} has guessed Coinflip correctly ${person.correct} time${person.correct==1?'':'s'} and wrong ${person.wrong} time${person.wrong==1?'':'s'}!`
+
+    } else {
+
+      // User has not played.
+      str = `${id} has not played Coinflip!`;
+
+    }
+
+    // Return response.
+    return str;
+
+  } catch (err) {
+    console.log(err);
+    return;
+  }
+};
+
+
+// Function to get the leaderboard.
+async function coinflipLb() {
+  try {
+    
+    // Pull top users from the database.
+    let client = await pool.connect();
+    let res = await client.query(`(SELECT user_id, correct, wrong FROM coinflip WHERE correct = (SELECT MAX (correct) FROM coinflip) LIMIT 1) UNION ALL (SELECT user_id, correct, wrong FROM coinflip WHERE wrong = (SELECT MAX (wrong) FROM coinflip) LIMIT 1) ORDER BY correct DESC;`);
+    let top = res.rows;
+    client.release();
+
+    // If there's just one user, they have both high scores.
+    if (top.length < 2) {
+      top.push(top[0]);
+    }
+
+    // Return response.
+    return `Coinflip Leaderboard | Top Correct: ${top[0].user_id} (${top[0].correct}) | Top Wrong: ${top[1].user_id} (${top[1].wrong})`;
+
+  } catch (err) {
+    console.log(err);
+    return;
+  }
+}
+
+
+export { coinflip, coinflipScore, coinflipLb };

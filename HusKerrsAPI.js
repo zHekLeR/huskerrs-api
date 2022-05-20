4a66c26bcafdaaa8c9a486b55f9d92accf809c53
zHekLeR <zHekLeR@hotmail.com>
2022-05-20 17:25:27 -0400

    RRLB

--- HusKerrsAPI.js
+++ HusKerrsAPI.js
@@ -548,7 +548,7 @@ bot.on('chat', async (channel, tags, message) => {
         break;
       
       case '!check':
-        if (channel.substring(1) !== 'huskerrs' || (!tags['mod'] || !vips.includes(tags['username']))) break;
+        if (channel.substring(1) !== 'huskerrs' || (!tags['mod'] && !vips.includes(tags['username']))) break;
         bot.say(channel, await stats(encodeURIComponent(message.substring(message.indexOf(' ') + 1))));
         break;
 
